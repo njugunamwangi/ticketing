e263ac9161a1edec060e0b555bd66c42d4edf149
njugunamwangi <njugunamwangi.n@gmail.com>
2024-11-17 17:47:56 +0300

    Authentication using tokens

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -2,14 +2,25 @@
 
 namespace App\Traits;
 
+use Illuminate\Http\Response;
+
 trait ApiResponses
 {
-    protected function ok($message)
+    protected function ok($message, $data)
     {
-        return $this->success($message, 200);
+        return $this->success($message, $data, Response::HTTP_ACCEPTED);
     }
 
-    protected function success($message, $statusCode = 200)
+    protected function success($message, $data, $statusCode = Response::HTTP_ACCEPTED)
+    {
+        return response()->json([
+            'message' => $message,
+            'data' => $data,
+            'statusCode' => $statusCode,
+        ], $statusCode);
+    }
+
+    protected function error($message, $statusCode)
     {
         return response()->json([
             'message' => $message,
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -6,4 +6,5 @@ use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')
+    ->apiResource('tickets', TicketController::class);
