0cc0555de9e902805d1717eb7653bf6d5c121dab
njugunamwangi <njugunamwangi.n@gmail.com>
2024-11-18 21:29:42 +0300

    Conditionally omitting & including data

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')
-    ->apiResource('tickets', TicketController::class);
+    ->group(function() {
+
+        Route::apiResource('tickets', TicketController::class);
+        Route::apiResource('users', UserController::class);
+
+    });
+
